fb92e5561beefc8f828b22ae1ea1e3cc70705566
safdar ayub <safdarayub@gmail.com>
2025-07-27 23:51:36 +0500

    third

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import prisma from '@/lib/prisma'
 
-const Homepage = () => {
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
-    <div>MY first Hello world page. And I am using Next.js 15 and Prisma 4.</div>
-  )
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
-
-export default Homepage
