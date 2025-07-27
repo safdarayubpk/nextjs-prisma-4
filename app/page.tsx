ac26fc600b867abd9b10b41518e9fe6fce8b9cab
safdar ayub <safdarayub@gmail.com>
2025-07-27 23:53:21 +0500

    fourth

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import prisma from '@/lib/prisma'
 export default async function Home() {
   const users = await prisma.user.findMany();
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+    <div className="min-h-screen bg-gray-150 flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
         Superblog
       </h1>
