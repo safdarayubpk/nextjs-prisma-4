6b42a3e787f2d4665e4a52fd737331ca073776fe
safdar ayub <safdarayub@gmail.com>
2025-07-27 23:30:30 +0500

    Second

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>MY first Hello world page.</div>
+    <div>MY first Hello world page. And I am using Next.js 15 and Prisma 4.</div>
   )
 }
 
